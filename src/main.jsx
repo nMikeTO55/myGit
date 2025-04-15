24255df3691586203ebad6ff491922a0c18e9eba
Mike <mike.j.parish@gmail.com>
2025-04-15 09:55:57 -0400

    begin loading files

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
+import {UserProvider} from './contexts/user.context.jsx';
+import { CategoriesProvider } from "./contexts/categories.context.jsx";
+import {CartProvider} from './contexts/cart.context.jsx';
+
+import "./index.scss";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
